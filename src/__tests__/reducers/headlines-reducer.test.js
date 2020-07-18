a880342349703921d0e16884d953c95fc004f113
Allison Mackey <a.mackey693@gmail.com>
2020-07-18 14:03:22 -0700

    add #headlinesReducer test 'requesting headlines should successfully change isLoading from false to true' test fails as expected

--- src/__tests__/reducers/headlines-reducer.test.js
+++ src/__tests__/reducers/headlines-reducer.test.js
@@ -1,11 +1,24 @@
 import headlinesReducer from '../../reducers/headlines-reducer';
+import * as c from './../../actions/ActionTypes'
 
 describe('headlinesReducer', () => {
+  let action;
   const defaultState = {
     isLoading: false, 
     headlines: [],
     error: null
   };
+
+  test('requesting headlines should successfully change isLoading from false to true', () => {
+    action = {
+      type: c.REQUEST_HEADLINES
+    };
+    expect(headlinesReducer(defaultState, action)).toEqual({
+      isLoading: true, 
+      headlines: [],
+      error: null
+    });
+  });
 
   test('should successfully return the default state if no action is passed into it', () => {
     expect(headlinesReducer(defaultState, {type:null})).toEqual(
