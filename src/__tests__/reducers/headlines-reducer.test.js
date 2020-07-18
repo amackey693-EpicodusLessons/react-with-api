c69b0befbec93f491ce48613a3d280f31a71eec4
Allison Mackey <a.mackey693@gmail.com>
2020-07-18 14:09:18 -0700

    add #headlinesReducer test 'successfully getting headlines should change isLoading to false and update headlines' test fails as expected

--- src/__tests__/reducers/headlines-reducer.test.js
+++ src/__tests__/reducers/headlines-reducer.test.js
@@ -9,6 +9,25 @@ describe('headlinesReducer', () => {
     error: null
   };
 
+  const loadingState = {
+    isLoading: false, 
+    headlines: [],
+    error: null
+  };
+
+  test('successfully getting headlines should change isLoading to false and update headliens', () => {
+    const headlines = "A headline";
+    action = {
+      type: c.GET_HEADLINES_SUCCESS,
+      headlines
+    }
+    expect(headlinesReducer(loadingState, action)).toEqual({
+      isLoading: false, 
+      headlines: "A headline",
+      error: null
+    });
+  });
+  
   test('requesting headlines should successfully change isLoading from false to true', () => {
     action = {
       type: c.REQUEST_HEADLINES
