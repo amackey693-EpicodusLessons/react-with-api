f2033ccd0e626ca0244a490a66c6f67679cfafc3
Allison Mackey <a.mackey693@gmail.com>
2020-07-20 10:49:43 -0700

    add middleware to project and finalize weekend homework

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import thunkMiddleware from 'redux-thunk';
+import middleWareLogger from './middleware/middleware-logger';
+import {createStore, applyMiddleware} from 'redux';
+import reducer from './reducers/headlines-reducer';
+import {Provider} from 'react-redux';
+import middlewareLogger from './middleware/middleware-logger';
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, middlewareLogger));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
